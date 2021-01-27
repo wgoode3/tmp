dee634b72341d2f2bd53a5f7a8f651322491dac4
wgoode3 <wgoode3@gmail.com>
2021-01-27 17:21:36 -0500

    Create sig.js

---
+++ sig.js
@@ -0,0 +1,6 @@
+{ "name": "Marisa Goode", 
+  "pronouns": ["she", "her", "hers"], 
+  "title": "Lead Instructor", 
+  "location": "Arlington, VA" }
+// CodingDojo.com
+// Transforming lives through programming literacy.
